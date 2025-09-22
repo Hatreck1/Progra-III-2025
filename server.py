69fb5926b1eeef8ce16832fa87257470d4f1d52a
Hatreck1 <Hatreck1@gmail.com>
2025-09-22 14:56:55 -0600

    inclusion de crud academico

--- server.py
+++ server.py
@@ -1,11 +1,30 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
+from urllib import parse
+import json 
+import crud_alumno
+
 port = 3000
+
+crudAlumno = crud_alumno.crud_alumno()
+
 class miServidor(SimpleHTTPRequestHandler):
     def do_GET(self):
-        if self.path == "/":
-            self.path = "/index.html"
-        return SimpleHTTPRequestHandler.do_GET(self)
+        if self.path=="/":
+            self.path="index.html"
+            return SimpleHTTPRequestHandler.do_GET(self)
     
-print("Servidor ejecutandose en puerto 3000")
+    def do_POST(self):
+        longitud = int(self.headers['Content-Length'])
+        datos = self.rfile.read(longitud)
+        datos = datos.decode("utf-8")
+        datos = parse.unquote(datos)
+        datos = json.loads(datos)
+        resp = {"msg": crudAlumno.administrar(datos)}
+        
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(json.dumps(resp).encode("utf-8"))
+
+print("Servidor ejecutandose en el puerto", port)
 server = HTTPServer(("localhost", port), miServidor)
 server.serve_forever()
